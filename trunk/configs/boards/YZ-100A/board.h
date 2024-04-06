944da8c339a9697fb8fed940f76c48017c93bf26
bqtlq <32087928+bqtlq@users.noreply.github.com>
2024-04-06 15:10:07 +0800

    Add files via upload

---
+++ trunk/configs/boards/YZ-100A/board.h
@@ -0,0 +1,29 @@
+/* YZ-100A */
+
+#define BOARD_PID		"YZ-100A"
+#define BOARD_NAME		"YZ-100A"
+#define BOARD_DESC		"YZ-100A Wireless Router"
+#define BOARD_VENDOR_NAME	"LQDN"
+#define BOARD_VENDOR_URL	"https://github.com/bqtlq"
+#define BOARD_MODEL_URL		"https://github.com/bqtlq"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	2
+#define BOARD_GPIO_BTN_WPS	1
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	39
+#undef BOARD_GPIO_LED_POWER	9
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB	11
+#undef BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB
+#define BOARD_ROLE_REPEATER	1
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
